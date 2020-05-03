c0d4c565ab3979cb3ccef837c7e4a95cccc43715
Prashambhuta <prasham.bhuta@gmail.com>
2020-05-03 14:38:29 +0530

    Lecture 5 Source Code

---
+++ cs50_web/source/src5/currency/application.py
@@ -0,0 +1,33 @@
+import requests, flask
+
+from flask import Flask, jsonify, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/convert", methods=["POST"])
+def convert():
+
+    # Query for currency exchange rate
+    currency = request.form.get("currency")
+    res = requests.get("https://api.exchangeratesapi.io/latest", params={
+        "base": "USD"})
+
+    # Make sure request succeeded
+    if res.status_code != 200:
+        return jsonify({"success": False})
+
+    # Make sure currency is in response
+    data = res.json()
+    if currency not in data["rates"]:
+        return jsonify({"success": False})
+
+    return jsonify({"success": True, "rate": data["rates"][currency]})
+
+if __name__ == "__main__":
+    app.run()
